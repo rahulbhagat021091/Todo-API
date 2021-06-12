e3073fd42c079c656785906249724df73bc82466
Rahul Bhagat <rahul.tribal123@gmail.com>
2021-06-12 16:38:34 +0530

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore')
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -18,13 +19,13 @@ app.get('/todos', function (req, res) {
 
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo =_.findWhere(todos, {id : todosId}); 
+	// var matchedTodo;
+	// todos.forEach(function (todo) {
+	// 	if (todoId === todo.id) {			
+	// 		matchedTodo = todo;
+	// 	}
+	// });
 		if (matchedTodo) {
 			res.json(matchedTodo);
 		}	else	{
@@ -32,11 +33,17 @@ app.get('/todos/:id', function (req, res) {
 		}
 });
 
-app.post('/todos', function (req, res) {
-		var body = req.body;
+		app.post('/todos', function (req, res) {
+		var body = _.pick(req.body, 'description', 'completed');
+
+		if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {  
+			return res.status(404).send(); 
+		}
+		
+		body.description = body.description.trim();
 
 		body.id = todoNextId++;
-		
+		  
 		todos.push(body);
 		
 		res.json(body);
